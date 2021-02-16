779a3abfc6ee7cf8060c8c7119431ca51f0d9892
Kirill <ka4ernov@gmail.com>
2021-02-16 20:55:05 +0300

    first version of personal movies database

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('How many movies have you seen?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastMovieFirst = prompt('Name a movie you have seen lately', ''),
+      lastMovieScoreFirst = prompt('How do you rate this movie?', ''),
+      lastMovieSecond = prompt('Name a movie you have seen lately?', ''),
+      lastMovieScoreSecond = prompt('How do you rate this movie?', '');
+
+personalMovieDB.movies[lastMovieFirst] = lastMovieScoreFirst;
+personalMovieDB.movies[lastMovieSecond] = lastMovieScoreSecond;
+
+console.log(personalMovieDB);
